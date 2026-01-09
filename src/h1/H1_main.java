31bd39f75451147d88bdf4a118c8ffb53e425a40
Nijat89 <nijatamirli610@gmail.com>
2026-01-09 20:19:06 +0100

    Übung10

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -3,41 +3,5 @@ package h1;
 public class H1_main {
 	public static void main(String[] args) {
 		
-		/*
-		// Beispiel aus der Aufgabenstellung
-		int rows = 4;
-		int cols = 5;
-
-		Cell[] startConfig = new Cell[] { new Cell(0, 4), new Cell(1, 1), new Cell(2, 2), new Cell(2, 3),
-				new Cell(3, 1), new Cell(3, 2) };
-
-		Grid game = new Grid(startConfig, rows, cols);
-
-		System.out.println("--- Start Konfiguration ---");
-		printGrid(game);
-
-		System.out.println("\n--- Generation 1 ---");
-		game.computeGeneration(1);
-		printGrid(game);
-
-		System.out.println("\n--- Generation 2 (von Start aus gesehen) ---");
-		game.computeGeneration(1);
-		printGrid(game);
-	}
-
-	// Hilfsmethode zur Ausgabe auf der Konsole
-	public static void printGrid(Grid grid) {
-		Cell[][] cells = grid.getGridArray();
-		for (int r = 0; r < cells.length; r++) {
-			for (int c = 0; c < cells[0].length; c++) {
-				System.out.print(cells[r][c].isAlive() ? "1 " : "0 ");
-			}
-			System.out.println();
-		}
-		
-		*/
-		
-		
-		
 	}
 }
