e0feabfde552e230fad9a7fafac0323b530748e1
Nijat89 <nijatamirli610@gmail.com>
2026-01-09 20:19:34 +0100

    Übung10

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,32 +1,8 @@
 package h2;
 
 public class H2_main {
-
 	public static void main(String[] args) {
+
 		
-		/*
-		// Beispiel 1
-		int dim = 5;
-		Spielbrett sb = new Spielbrett(dim);
-		Feld[][] brett = sb.getBrett();
-
-		String[] directions = { "URLLD", "LRLLD", "ULLLR", "DRULL", "URLDD" };
-
-		for (int r = 0; r < dim; r++) {
-			for (int c = 0; c < dim; c++) {
-				char dir = directions[r].charAt(c);
-				brett[r][c] = new Feld(false, dir);
-			}
-		}
-
-		Spielstein stein = new Spielstein(sb, 3, 2);
-		stein.go(6);
-
-		System.out.println("Endposition: " + stein.getCurrentRow() + ", " + stein.getCurrentCol());
-		
-		*/
-		
-		
-
 	}
 }
